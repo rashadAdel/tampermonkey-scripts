bf8870249bdaa4057217d2ea618257b1a77929ad
rashadAdel <rashad-adel@outlook.com>
2026-05-25 21:40:11 +0300

    init

---
+++ loader.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         My Scripts Loader
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Loads scripts from GitHub based on current URL
+// @match        *://*/*
+// @grant        GM_xmlhttpRequest
+// @connect      raw.githubusercontent.com
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    const CONFIG_URL = 'https://raw.githubusercontent.com/rashadAdel/tampermonkey-scripts/main/config.json';
+
+    function matchesPattern(pattern, url) {
+        const regex = new RegExp('^' + pattern.replace(/\*/g, '.*') + '$');
+        return regex.test(url);
+    }
+
+    GM_xmlhttpRequest({
+        method: 'GET',
+        url: CONFIG_URL,
+        onload: function (res) {
+            const configs = JSON.parse(res.responseText);
+            const currentURL = window.location.href;
+
+            configs.forEach(cfg => {
+                if (matchesPattern(cfg.match, currentURL)) {
+                    GM_xmlhttpRequest({
+                        method: 'GET',
+                        url: cfg.script,
+                        onload: function (r) {
+                            eval(r.responseText);
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+})();
---
+++ scripts/greenline.js
@@ -0,0 +1,29 @@
+(function () {
+    'use strict';
+
+    function attachEnterListener() {
+        const input = document.getElementById('orderId');
+        if (!input) return;
+
+        input.addEventListener('keydown', function (e) {
+            if (e.key === 'Enter') {
+                e.preventDefault();
+                findOrders();
+            }
+        });
+    }
+
+    const observer = new MutationObserver(() => {
+        if (document.getElementById('orderId')) {
+            attachEnterListener();
+            observer.disconnect();
+        }
+    });
+
+    if (document.getElementById('orderId')) {
+        attachEnterListener();
+    } else {
+        observer.observe(document.body, { childList: true, subtree: true });
+    }
+
+})();
