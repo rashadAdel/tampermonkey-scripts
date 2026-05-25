4158231f9bd119c815e1e7e14022973ba414532b
rashadAdel <rashad-adel@outlook.com>
2026-05-25 21:49:38 +0300

    #

--- loader.user.js
+++ loader.user.js
@@ -1,42 +1,44 @@
 // ==UserScript==
-// @name         My Scripts Loader
+// @name         Greenline Script Loader
+// @author       Rashad Adel
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Loads scripts from GitHub based on current URL
+// @icon         https://system.greenlineco.com/app-assets/images/logo/GreenLineLogo.png
 // @match        *://*/*
 // @grant        GM_xmlhttpRequest
 // @connect      raw.githubusercontent.com
 // ==/UserScript==
 
 (function () {
-    'use strict';
+  "use strict";
 
-    const CONFIG_URL = 'https://raw.githubusercontent.com/rashadAdel/tampermonkey-scripts/main/config.json';
+  const CONFIG_URL =
+    "https://raw.githubusercontent.com/rashadAdel/tampermonkey-scripts/main/config.json";
 
-    function matchesPattern(pattern, url) {
-        const regex = new RegExp('^' + pattern.replace(/\*/g, '.*') + '$');
-        return regex.test(url);
-    }
+  function matchesPattern(pattern, url) {
+    const regex = new RegExp("^" + pattern.replace(/\*/g, ".*") + "$");
+    return regex.test(url);
+  }
 
-    GM_xmlhttpRequest({
-        method: 'GET',
-        url: CONFIG_URL,
-        onload: function (res) {
-            const configs = JSON.parse(res.responseText);
-            const currentURL = window.location.href;
+  GM_xmlhttpRequest({
+    method: "GET",
+    url: CONFIG_URL,
+    onload: function (res) {
+      const configs = JSON.parse(res.responseText);
+      const currentURL = window.location.href;
 
-            configs.forEach(cfg => {
-                if (matchesPattern(cfg.match, currentURL)) {
-                    GM_xmlhttpRequest({
-                        method: 'GET',
-                        url: cfg.script,
-                        onload: function (r) {
-                            eval(r.responseText);
-                        }
-                    });
-                }
-            });
+      configs.forEach((cfg) => {
+        if (matchesPattern(cfg.match, currentURL)) {
+          GM_xmlhttpRequest({
+            method: "GET",
+            url: cfg.script,
+            onload: function (r) {
+              eval(r.responseText);
+            },
+          });
         }
-    });
-
+      });
+    },
+  });
 })();
