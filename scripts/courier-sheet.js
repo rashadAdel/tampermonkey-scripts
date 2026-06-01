cb364ee97218b80e622fd7217835b324f0bd143f
rashadAdel <rashad-adel@outlook.com>
2026-06-02 02:16:47 +0300

    #

--- scripts/courier-sheet.js
+++ scripts/courier-sheet.js
@@ -311,7 +311,7 @@
           downloadData,
           courierName + "_orders_" + today.replace(/\//g, "-"),
         );
-        sendToSheets(downloadData.splice(1), courierName);
+        sendToSheets(downloadData.slice(1), courierName);
         await assignCoureir();
       });
   }
