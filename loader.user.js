42cbfb375063f05c4fc9aa5620d54c40bfcb42de
rashadAdel <rashad-adel@outlook.com>
2026-06-23 18:58:28 +0300

    #

--- loader.user.js
+++ loader.user.js
@@ -4,7 +4,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      2.9.2
 // @description  Loads scripts from GitHub based on current URL
-// @icon         https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/327165164_685846989996055_4420915704404091060_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=LG8yvWAdkp0Q7kNvwEH_c8t&_nc_oc=AdolowdYmy3fX0lA-2IGpknhjUV8dSUc2KCRZCRz6rFdDM8da8SX6rN3QzPD-r_r_B0&_nc_zt=23&_nc_ht=scontent.fcai19-6.fna&_nc_gid=pt2kgmMNlz59yFmO_cum7A&_nc_ss=7b2a8&oh=00_Af5c5RzFKCmqGIr3txXrGs9yb4wL13kGpdm5IxP__umwwA&oe=6A1A7AE9
+// @icon         https://system.greenlineco.com/app-assets/images/logo/logo.png
 // @match        *://*/*
 // @grant        GM_xmlhttpRequest
 // @connect      raw.githubusercontent.com
